39fe19ef6132b7cd76d9025abe536ddc76a38941
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-05 22:33:39 +0100

    page layout done and responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Button variant="solid" colorScheme={'blue'}>Button</Button>;
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={isLargeScreen}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
