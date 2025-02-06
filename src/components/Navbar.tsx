3b9c79f241be7c2e9dbe35acdd71f47002068134
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-06 08:53:15 +0100

    darkmode implemented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, Text } from "@chakra-ui/react";
+import { useColorMode } from "./ui/color-mode";
 import logo from "../assets/logo.webp";
 const Navbar = () => {
+  const { toggleColorMode } = useColorMode();
   return (
     <HStack>
       <Image src={logo} boxSize={"50px"} padding={"5px"} />
       <Text>Navbar</Text>
+      <Button onClick={toggleColorMode}>Change Theme</Button>
     </HStack>
   );
 };
