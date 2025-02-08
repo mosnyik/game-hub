e71a2fd7507e380fa09a5e15dcc0fae048ccf862
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-08 09:45:04 +0100

    feat: Created a sort selector

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,25 +1,32 @@
-import { Button, HStack, MenuContent, MenuItem, MenuRoot, MenuTrigger } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import PlatformSelectorSkeleton from "./PlatformSelectorSkeleton";
 
 const SortSelector = () => {
-//  const { data, error, isLoading } = usePlatforms();
+  //  const { data, error, isLoading } = usePlatforms();
 
-//  if (error) return null;
- return (
-   <MenuRoot>
-     <MenuTrigger asChild>
-       <Button variant="outline" size="sm" marginBottom={1}>
-         <HStack>
-           {/* {selectedPlatform ? selectedPlatform.name : <span>Platforms</span>} */}
-
-           <BsChevronDown />
-         </HStack>
-       </Button>
-     </MenuTrigger>
-     <MenuContent position="absolute" zIndex="10" borderRadius={"5px"}>
-       {/* {isLoading && <PlatformSelectorSkeleton />}  */}
-       {/* {data.map((platform) => (
+  //  if (error) return null;
+  return (
+    <MenuRoot>
+      <MenuTrigger asChild>
+        <Button variant="outline" size="sm" marginBottom={1}>
+          <HStack>
+            {/* {selectedPlatform ? selectedPlatform.name : <span>Platforms</span>} */}
+            Order By: Relevance
+            <BsChevronDown />
+          </HStack>
+        </Button>
+      </MenuTrigger>
+      <MenuContent position="relative" zIndex="10" borderRadius={"5px"}>
+        {/* {isLoading && <PlatformSelectorSkeleton />}  */}
+        {/* {data.map((platform) => (
          <MenuItem
            value={platform.name}
            key={platform.id}
@@ -28,10 +35,10 @@ const SortSelector = () => {
            {platform.name}
          </MenuItem>
        ))} */}
-       
-     </MenuContent>
-   </MenuRoot>
- );
+        <MenuItem value={"relevance"}>Relevance</MenuItem>
+      </MenuContent>
+    </MenuRoot>
+  );
 };
 
 export default SortSelector;
